5b3087c9e49a9d4206f863ffc6b697396995d3ec
SabbathCode <daniel.arturjsb@gmail.com>
2022-05-04 15:31:33 -0300

    creating first objects

--- marine.java
+++ marine.java
@@ -1,37 +1,64 @@
 import java.util.Scanner;
 
-class pirate{
+class Pirate{
     String name;
     String nick;
-
     String akumanomi;
     String ocupation;
     String tripulation;
+    boolean status;
     int reward;
+}
+
+class tripulation{
+    String name;
+    String Captain;
+    String members[];
+    String jobs[];
 }
 
 public class marine{
 
-    static void addPirate(pirate){
+    static void addPirate(){
         Scanner sc= new Scanner(System.in); 
         System.out.print("Enter a full pirate name: ");  
         String fullname= sc.nextLine();
     }
 
     static void showPirate(){
-        System.out.println("Your full name is:" + name);
-        System.out.println("know as:" + name);
-        System.out.println("Your reward is:" + reward);
+        System.out.println("Your full name is:");
+        System.out.println("know as:");
+        System.out.println("Your reward is:");
         System.out.println("===== more informations =====");
-        System.out.print(" .");
+        System.out.print(".");
         
     }
     
     static void capturePirate(){
-        System.out.println("The Notorius pirate" +name "has been captured");
+        System.out.println("The Notorius pirate");
     }
 
     public static void main(String[] args){
-        -addPirate();
+        //Create my first object
+
+        Pirate luffy = new Pirate();
+
+        luffy.name = "Monkey D. Luffy";
+        luffy.nick = "Mugiwara no Luffy";
+        luffy.akumanomi = "Hito Hito no mi model God Sun";
+        luffy.ocupation = "Captain";
+        luffy.reward = 1500000000;
+
+        Pirate sanji = new Pirate();
+
+        sanji.name = "Vinsmoke Sanji";
+        sanji.nick = "Black Leg Sanji";
+        sanji.akumanomi = "no have";
+        sanji.ocupation = "Cooker";
+        sanji.reward = 330000000;
+
+
+        capturePirate();
+
     }
 }
