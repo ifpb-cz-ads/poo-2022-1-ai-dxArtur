6773c1cf9a410f906cc753363bcfc8f226231575
SabbathCode <daniel.arturjsb@gmail.com>
2022-06-03 07:11:34 -0300

    incrementing database

--- marine.java
+++ marine.java
@@ -7,6 +7,8 @@ class Peaple{
 }
 // as classes Pirate e Sailor são classes abastratas de pessoas 
 
+
+
 class Pirate{
     String name;
     String nick;
@@ -38,21 +40,29 @@ public class marine{
         String fullname= sc.nextLine();
     }
 
-    static void moveTripulation(){
+    static void moveTripulation(Pirate pirate, Tripulation tripulation){
         System.out.print("Now, the pirate of the tripulation is a ocupation on the board tripulation");
     }
 
     static void showPirate(Pirate pirate){
-        System.out.println("Your full name is:" + pirate.name);
-        System.out.println("know as:" + pirate.nick);
-        System.out.println("Your reward is:" + pirate.reward);
+        System.out.println("Your full name is: " + pirate.name);
+        System.out.println("know as: " + pirate.nick);
+        System.out.println("Your reward is: " + pirate.reward);
+        System.out.println("");
         System.out.println("===== more informations =====");
-        System.out.println("Actually is: " + pirate.ocupation + "on board on: " + pirate.tripulation.ship);
+        System.out.println("");
+        System.out.println("Actually is: " + pirate.ocupation + " on board on: " + pirate.tripulation.ship);
+        System.out.println("");
         
     }
     
     static void capturePirate(Pirate pirate){
-        System.out.println("The Notorius pirate");
+        if(pirate.status != true){
+            System.out.println("The Notorius pirate " + pirate.nick + " was captured");
+        } else {
+            System.out.println("The Pirate " + pirate.nick + " is already captured");
+        }
+
     }
 
     public static void main(String[] args){
@@ -83,11 +93,13 @@ public class marine{
         sanji.nick = "Black Leg Sanji";
         sanji.akumanomi = "no have";
         sanji.ocupation = "Cooker";
+        sanji.tripulation = mugiwara;
         sanji.reward = 330000000;
 
 
         
         showPirate(luffy);
+        showPirate(sanji);
         capturePirate(luffy);
 
         //Scanner input = new Scanner(System.in);
