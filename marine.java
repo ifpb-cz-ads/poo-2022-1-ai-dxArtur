72d73ddd5d9d2dff38ed38e390a734a3cdfd6790
SabbathCode <daniel.arturjsb@gmail.com>
2022-08-04 11:59:53 -0300

    updating project

--- marine.java
+++ marine.java
@@ -1,20 +1,24 @@
+import java.lang.reflect.Member;
 import java.util.Scanner;
 
-public abstract class Peaple{
+abstract class Peaple{
     protected String name;
     protected String nick;
     protected String akumanomi;
     protected String haki;
 
-    public Peaple(){
-    this.name = name;
-    this.nick = nick;
-    this.akumanomi = akumanomi;
-    this.haki = haki;
+    public Peaple(String name, String nick, String akumanomi, String haki){
+        this.name = name;
+        this.nick = nick;
+        this.akumanomi = akumanomi;
+        this.haki = haki;
     }
     
+    public Peaple() {
+    }
+
     public String getName(){
-        return name;
+        return this.name;
     }
     
     public void setName(String name){
@@ -22,7 +26,7 @@ public abstract class Peaple{
     }
     
     public String getNick(){
-        return nick;
+        return this.nick;
     }
     
     public void setNick(String nick){
@@ -30,7 +34,7 @@ public abstract class Peaple{
     }
     
     public String getAkumanomi(){
-        return akumanomi;
+        return this.akumanomi;
     }
     
     public void setAkumanomi(String akumanomi){
@@ -38,7 +42,7 @@ public abstract class Peaple{
     }
     
     public String getHaki(){
-        return haki;
+        return this.haki;
     }
     
     public void setHaki(String haki){
@@ -48,62 +52,164 @@ public abstract class Peaple{
 }
 // as classes Pirate e Sailor são classes abstratas de Peaple 
 
+class Tripulation{
+    protected String name;
+    protected Pirate[] members;
+    //protected String[] jobs = {"Captain", "Chief officer", "Cooker", "Navigator", "Pilot", "Sniper", "Musician", "Doctor", "Archaeologist", "Shipwright", "Crew Members"};
+    protected String ship;
 
+    public Tripulation(){
+
+    }
+
+    public Tripulation(String name, Pirate[] members, String ship){
+        this.name = name;
+        this.members = members;
+        //how to define like a atributte one array of objects
+        this.ship = ship;
+    }
+
+    
+
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Pirate[] getMembers(){
+        return this.members;
+    }
+    
+    public void setMembers(Pirate members[]){
+        this.members = members;
+    }
+
+    public String getShip(){
+        return this.ship;
+    }
+
+    public void setShip(String ship){
+        this.ship = ship;
+    }
+
+    
+}
 
 class Pirate extends Peaple{
+    
     protected String ocupation;
-    protected Tripulation tripulation;
     protected boolean status;
     protected int reward;
+    protected Tripulation tripulation;
     
     public Pirate(){
-    this.ocupation = ocupation;
-    this.status = status;
-    this.reward = reward;
-    this.tripulation = tripulation;
-    // create other builder
+        super();
+    }
+
+    public Pirate(String name, String nick, String akumanomi, String haki, String ocupation, boolean status, int reward){
+        super(name, nick, akumanomi, haki);
+        this.ocupation = ocupation;
+        this.status = status;
+        this.reward = reward;
+        this.tripulation = tripulation;
+    
+    
+    }
+
+    public String getOcupation(){
+        return this.ocupation;
+    }
+
+    public void setOcupation(String ocupation){
+        this.ocupation = ocupation;
+    }
+
+    public Boolean getStatus(){
+        return this.status;
+    }
+
+    public void setStatus(Boolean status){
+        this.status = status;
+    }
+
+    public int getReward(){
+        return this.reward;
+    }
+
+    public void setReward(int reward){
+        this.reward = reward;
+    }
+
+    
+    public Pirate(String name, String nick, String akumanomi, String haki, String ocupation, boolean status, int reward, Tripulation tripulation){
+        super(name, nick, akumanomi, haki);
+        this.ocupation = ocupation;
+        this.status = status;
+        this.reward = reward;
+        this.tripulation = tripulation;
+    }
+
+    public String tripulationNull(){
+        return "No have tripulation";
+    }
+
+    public Tripulation getTripulation(){
+        if(this.tripulation == null){
+            tripulationNull();
+        }else{
+            return this.tripulation;
+        }
+        return this.tripulation;
+    }
+
+    public void setTripulation(Tripulation tripulation){
+        this.tripulation=tripulation;
     }
 }
 
 class Sailor extends Peaple{
     protected String ocupation;
-    
-    public Sailor(){
-    this.ocupation = ocupation;
+
+    public Sailor(String name, String nick, String akumanomi, String haki, String ocupation){
+        super(name, nick, akumanomi, haki);
+        this.ocupation = ocupation;
+    }
+
+    public String getOcupation(){
+        return this.ocupation;
+    }
+    public void setOcupation(String ocupation){
+        this.ocupation = ocupation;
     }
 }
 
-class Tripulation{
-    String name;
-    String[][] membersJobs;
-    Pirate[] members;
-    String[] jobs = {"Captain", "Chief officer", "Cooker", "Navigator", "Pilot", "Sniper", "Musician", "Doctor", "Archaeologist", "Shipwright", "Crew Members"};
-    String ship;
-}
+
 
 
 
 public class marine{
 
-    static void addPirate(){
-        Scanner sc= new Scanner(System.in); 
-        System.out.print("Enter a full pirate name: ");  
-        String fullname= sc.nextLine();
-    }
-
+    
     static void moveTripulation(Pirate pirate, Tripulation tripulation){
         //for
         System.out.print("Now, the pirate of the tripulation is a ocupation on the board tripulation");
     }
 
     static void showPirate(Pirate pirate){
-        System.out.println("Your full name is: " + pirate.name);
-        System.out.println("know as: " + pirate.nick);
-        System.out.println("Your reward is: " + pirate.reward);
-        System.out.println("");
+        System.out.println("Your full name is: " + pirate.getName());
+        System.out.println("know as: " + pirate.getNick());
+        System.out.println("Your reward is: " + pirate.getReward());
         System.out.println("===== more informations =====");
-        //System.out.print("Actually is: " + pirate.ocupation + "on board on: " + pirate.tripulation.ship);//tripulation.name
+        //Pirate.tripulation = trpltn;
+        System.out.print("Actually is: " + pirate.getOcupation() + "on board on: " + pirate.tripulation.getShip());//tripulation.name
         
+    }
+
+    static void showSailor(Sailor sailor){
+
     }
     
     static void capturePirate(Pirate pirate){
@@ -115,19 +221,11 @@ public class marine{
 
     }
     
-    static void createTripulation(String tripulation){
-        
-        
-    }
 
     public static void main(String[] args){
         //Create my first object
-        Tripulation mugiwara = new Tripulation();
+        //Pirate luffy = new Pirate("Monkey D. Luffy", "Mugiwara no Luffy", "H");
 
-        mugiwara.name = "Mugiwara Pirates";
-        mugiwara.ship = "Thousand Sunny";
-        
-        
         Pirate luffy = new Pirate();
 
         luffy.name = "Monkey D. Luffy";
@@ -135,11 +233,6 @@ public class marine{
         luffy.akumanomi = "Hito Hito no mi model God Sun";
         luffy.ocupation = "Captain";
         luffy.reward = 1500000000;
-        luffy.tripulation.setName(mugiwara.name);
-        //como definir a tripulação dele: luffy.tripulation
-
-
-        System.out.print("");
 
         Pirate sanji = new Pirate();
 
@@ -147,16 +240,18 @@ public class marine{
         sanji.nick = "Black Leg Sanji";
         sanji.akumanomi = "no have";
         sanji.ocupation = "Cooker";
-        sanji.tripulation = mugiwara;
         sanji.reward = 330000000;
 
+        Tripulation mugiwara = new Tripulation();
 
+        mugiwara.name = "Mugiwara Pirates";
+        Pirate[] members = {luffy, sanji};
+        mugiwara.setMembers(members);
+        mugiwara.ship = "Thousand Sunny";
+ 
         
-
-        // como passar uma instancia como o parametro
         showPirate(luffy);
         showPirate(sanji);
-        //capturePirate(luffy);
 
  
         
